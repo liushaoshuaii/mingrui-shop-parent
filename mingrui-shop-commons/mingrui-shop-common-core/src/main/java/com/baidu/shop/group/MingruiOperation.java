1e2ccaf309c4e09dc2fed1f9b986f23a7a409e77
Seven <aaa>
2020-12-24 21:37:49 +0800

    aaa

---
+++ mingrui-shop-commons/mingrui-shop-common-core/src/main/java/com/baidu/shop/group/MingruiOperation.java
@@ -0,0 +1,13 @@
+package com.baidu.shop.group;
+
+/**
+ * @ClassName MingruiOperation
+ * @Description: TODO
+ * @Author lss
+ * @Date 2020/12/24
+ * @Version V1.0
+ **/
+public class MingruiOperation {
+    public interface Add{};
+    public interface Update{}
+}
